9979fafc3ed59f3abef5505c8d25d746ecac64d2
ksami <ksami.ihide@gmail.com>
2015-03-16 14:36:30 +0900

    use fspecial to generate gaussian kernel

--- test.m
+++ test.m
@@ -1,16 +1,12 @@
-path = '../data/img02.jpg';
-%path = 'test.jpg';
+%path = '../data/img02.jpg';
+path = 'test.jpg';
 img = imread(path);
 imshow(img);
 pause;
 
-gaussKernel = [0.006 0.061 0.242 0.383 0.242 0.061 0.006];
-%gaussKernel = [1 0 0; 0 1 0; 0 0 1];
+gaussKernel = fspecial('gaussian', 3, 1);
 img1 = myImageFilter(img, gaussKernel);
 %img1 = imfilter(img,gaussKernel,'replicate');
+
 imshow(img1);
 pause;
-
-gaussKernel = gaussKernel';
-img2 = myImageFilter(img1, gaussKernel);
-imshow(img2);
