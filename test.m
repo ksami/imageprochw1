60620d9de1020089f2aae682426984563f1c9ca9
ksami <ksami.ihide@gmail.com>
2015-03-19 13:35:02 +0900

    test file

--- test.m
+++ test.m
@@ -1,12 +1,20 @@
-path = '../data/img01.jpg';
+% path = 'orbiicon.jpg';
+path = '../data/img02.jpg';
 %path = 'test.jpg';
 img = imread(path);
 imshow(img);
 pause;
 
-[Im, Io, Ix, Iy] = myEdgeFilter(img, 1);
-subplot(1,2,1), subimage(Ix)
-subplot(1,2,2), subimage(Iy)
+[Im, Io, Ix, Iy] = myEdgeFilter(img, 2);
+
+% gaussKernel = fspecial('gaussian', [3 3], 2);
+% smooth = imfilter(img, gaussKernel, 'replicate');
+% sobelKernel = fspecial('sobel');
+% sobx = imfilter(smooth, sobelKernel, 'replicate');
+% soby = imfilter(smooth, sobelKernel', 'replicate');
+
+subplot(1,2,1), subimage(Im)
+subplot(1,2,2), subimage(Io)
 
 %gaussKernel = fspecial('gaussian', 3, 1);
 %img1 = myImageFilter(img, gaussKernel);  
