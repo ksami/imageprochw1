5aa5ceb4806ac19270e843602d6a75eac020796a
ksami <ksami.ihide@gmail.com>
2015-03-21 14:58:46 +0900

    add test file

--- test.m
+++ test.m
@@ -1,11 +1,20 @@
 % path = 'orbiicon.jpg';
 path = '../data/img02.jpg';
 %path = 'test.jpg';
+
+sigma     = 2;
+threshold = 0.03;
+rhoRes    = 2;
+thetaRes  = pi/180;
+nLines    = 20;
+
 img = imread(path);
 imshow(img);
 pause;
 
-[Im, Io, Ix, Iy] = myEdgeFilter(img, 2);
+[Im, Io, Ix, Iy] = myEdgeFilter(img, sigma);
+H = myHoughTransform(Im, threshold, rhoRes, thetaRes);
+[lineRho, lineTheta] = myHoughLines(H, rhoRes, thetaRes, nLines);
 
 % gaussKernel = fspecial('gaussian', [3 3], 2);
 % smooth = imfilter(img, gaussKernel, 'replicate');
@@ -13,8 +22,8 @@ pause;
 % sobx = imfilter(smooth, sobelKernel, 'replicate');
 % soby = imfilter(smooth, sobelKernel', 'replicate');
 
-subplot(1,2,1), subimage(Im)
-subplot(1,2,2), subimage(Io)
+% subplot(1,2,1), subimage(Im)
+% subplot(1,2,2), subimage(Io)
 
 %gaussKernel = fspecial('gaussian', 3, 1);
 %img1 = myImageFilter(img, gaussKernel);  
