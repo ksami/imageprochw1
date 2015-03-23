5a1949e629a843dfc1358cb18b1731d18dde26b6
ksami <ksami.ihide@gmail.com>
2015-03-24 01:32:51 +0900

    experiment conclusions

--- experiment.m
+++ experiment.m
@@ -7,11 +7,11 @@ thetaRes  = pi/180;
 nLines    = 20;
 
 %end of parameters
-i=3;
+i=6;
 
 % houghScript(sigma, threshold, rhoRes, thetaRes, nLines, i);  i=i+1;
 % houghScript(sigma, threshold, rhoRes, thetaRes, nLines*2, i);  i=i+1;
-houghScript(sigma-1, threshold, rhoRes, thetaRes, nLines, i);  i=i+1;
-houghScript(sigma, threshold-0.01, rhoRes, thetaRes, nLines, i);  i=i+1;
-houghScript(sigma, threshold, rhoRes-1, thetaRes, nLines, i);  i=i+1;
-% houghScript(sigma, threshold, rhoRes, thetaRes/2, nLines, i);  i=i+1;
+% houghScript(sigma-1, threshold, rhoRes, thetaRes, nLines, i);  i=i+1;
+% houghScript(sigma, threshold-0.01, rhoRes, thetaRes, nLines, i);  i=i+1;
+% houghScript(sigma, threshold, rhoRes-1, thetaRes, nLines, i);  i=i+1;
+houghScript(sigma+1, threshold, rhoRes, thetaRes, nLines, i);  i=i+1;
